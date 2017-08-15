547981e592ea5ae36db4284aeb609203b6a0b510
Sercan Ulucan <sercanulucan.dev@gmail.com>
2017-08-15 21:06:40 +0300

    test

--- assets/js/turnuva.js
+++ assets/js/turnuva.js
@@ -134,7 +134,7 @@ function writeGamestoTabs(resultsFileName, category, games) {
 
 function fillGamesCombobox(category, games) {
 
-    var text = "";
+    var text = "<option>Oyun seçiniz</option>";
     for (var tour = 1; tour < games.length; tour++) {
         for (var g = 0; g < games[tour].length; g++) {
 
